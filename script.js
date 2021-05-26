9fa02b1fc43b61573b677a44da6488a6eed55d80
Alexandra Murawski <almurawski15@gmail.com>
2021-05-25 23:38:05 -0400

    restarted js file code with better syntax

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //list of variables for easy access
 //Left search column
-let apiKey = "d423b4bd90678b82501d075774b8c2f7";
 let searchHistoryList = $(".search-history-list");
 let searchCityInput = $(".search-city");
 let searchCityButton = $(".search-city-button");
@@ -18,10 +17,60 @@ let fiveDayForecast = $(".five-day-forecast");
 let currentDate = moment().format('L');
 $("#current-date").text("(" + currentDate + ")"); 
 
+// start a search function to fetch data
 
-if (JSON.parse(localStorage.getItem("searchHistoryList")) === null) {
-    console.log("searchHistoryList not found")
-} else {
-    console.log("searchHistoryList loaded into searchHistoryArr");
-    renderSearchHistoryList();
+function startSearch (event) {
+    event.preventDefault();
+    
+    let userInput = document.querySelector("#search-value").value;
+
+    if (!userInput) {
+        console.log('Please enter a valid city!')
+        return;
+    } else {
+        getWeatherForecast (userInput);
+        getFiveDay (userInput);
+        saveSearch (userInput);
+        searchClear ();
+    }
 }
+
+// clear search function
+let searchClear = function () {
+    userInput.value = '';
+}
+
+// 
+
+
+// initiate button on click
+let buttonClicked = function (event) {
+    let newSearch = event.target.getAttribute('');
+
+    if (newSearch) {
+        getWeatherForecast(newSearch);
+        getFiveDay(newSearch);
+    }
+}
+
+let getWeatherForecast = function (city) {
+    let apiKey = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial' + '&appid=e1cda767f020cb0ebf52c83958433ba3';
+
+    fetch(apiKey)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayWeather(data,city);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('OpenWeatherMap not able to load')
+        });
+};
+
+
+searchCityButton.addEventListener('click', startSearch);
+clearHistoryButton.addEventListener('click', buttonClicked);
